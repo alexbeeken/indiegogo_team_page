c95dfc8f7e8ad5ad35fe649204a3c10943f3a8b7
Alex Beeken <aeb242@gmail.com>
2018-03-19 15:58:28 -0700

    saving work because I'm about to destroygit add app/views/svg/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,20 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require scrollReveal
+//= require jquery
+//= require jquery_ujs
+
+$(function() {
+  window.scrollReveal = new scrollReveal();
+});
+
+$(document).ready(function(){
+  var scroll_pos = 0;
+  $(document).scroll(function() {
+      scroll_pos = $(this).scrollTop();
+      if (scroll_pos > 210) {
+          $( ".skyline" ).css( "display", "none" );
+      }
+  });
+});
--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,4 +2,8 @@ class CampaignsController < ApplicationController
   def index
     @snapshot = ProjectSnapshot.latest
   end
+
+  def sunrise
+    @snapshot = ProjectSnapshot.latest
+  end
 end
